64fd024d0c4a55f9ea0dd0305a2efeaffdab0e01
Atihar Hossen Mahir <mahir_1990@yahoo.com>
2023-04-17 15:17:14 +0600

    explore updated

---
+++ pages/explore.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Eat from '../components/eats'
+import Footer from '../components/footer'
+import Hero from '../components/hero'
+import Map from '../components/map'
+import Stay from '../components/stay'
+import Head from 'next/head'
+
+function Explore() {
+  return (
+    <>
+    <Head>
+        <title>Explore Chakaria</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description"
+        content="Chakaria superb place with natural landscapes located 60km ahead of cox&apos;s-bazar. The hills of bandarban-alikadam and the Bay of Bengal has surrounded this area of 643.46 km2. Once there was a mangrove forest named Chakaria Sunderban here. But now it is no more. But yet this place has a lot of offerings to do and its turning 
+        into a popular tourist destination."/>
+        <meta name="keywords" content="chakaria, cox&apos;s bazar, chittagong division, chakaria shop, chiringa, alikodom, sonadiya, place to visit in Bangladesh, place to visit in cox&apos;s bazar, চকরিয়া, chiringa, chakaria news, chakaria tourist spot, chakaria hotel" />
+      </Head>
+    <Stay/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Explore
